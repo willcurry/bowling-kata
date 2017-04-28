ba6cc226d80146a4396f80d6269135180d639498
Will Curry <watercrafthd@gmail.com>
2017-04-28 13:18:51 +0100

    Score strike

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -11,4 +11,11 @@
   
   (it "scores a sprare correctly"
     (should= 16 
-      (score (concat '(5 5 3) (repeat 17 0))))))
+      (score (concat '(5 5 3) (repeat 17 0)))))
+
+  (it "scores a strike correctly"
+    (should= 26
+      (score (concat '(10 5 3) (repeat 17 0)))))
+
+  (it "scores perfect game correctly"
+    (should= 300 (score (repeat 20 10)))))
--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -3,13 +3,18 @@
 (defn- spare? [rolls]
   (= 10 (apply + (take 2 rolls))))
 
+(defn strike? [rolls]
+  (= 10 (first rolls)))
+
 (defn- rolls-to-score [rolls]
   (cond
     (spare? rolls) 3
+    (strike? rolls) 3
     :else 2))
 
 (defn- make-frames [rolls]
-  (lazy-seq (cons (take (rolls-to-score rolls) rolls) (make-frames (drop 2 rolls)))))
+  (lazy-seq (cons (take (rolls-to-score rolls) rolls) (make-frames (drop 
+    (if (strike? rolls) 1 2) rolls)))))
 
 (defn- make-game [rolls]
   (take 10 (make-frames rolls)))
