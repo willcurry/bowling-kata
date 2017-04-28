8a89ad1273aadee4c9726ef3a3dc4f34eac8d93d
Will Curry <watercrafthd@gmail.com>
2017-04-28 13:06:03 +0100

    Score spares

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -6,5 +6,9 @@
   (it "hitting the gutters gives 0 score"
     (should= 0 (score (repeat 20 0))))
   
-  (it "scores all ones with twenties"
-    (should= 20 (score (repeat 20 1)))))
+  (it "scores all rolls with twenties"
+    (should= 20 (score (repeat 20 1))))
+  
+  (it "scores a sprare correctly"
+    (should= 16 
+      (score (concat '(5 5 3) (repeat 17 0))))))
--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -1,4 +1,22 @@
 (ns bowling.core)
 
-(defn score [roles]
-  (reduce + roles))
+(defn- spare? [rolls]
+  (= 10 (apply + (take 2 rolls))))
+
+(defn- rolls-to-score [rolls]
+  (cond
+    (spare? rolls) 3
+    :else 2))
+
+(defn- make-frames [rolls]
+  (lazy-seq (cons (take (rolls-to-score rolls) rolls) (make-frames (drop 2 rolls)))))
+
+(defn- make-game [rolls]
+  (take 10 (make-frames rolls)))
+
+(defn- add-up [coll]
+  (reduce + coll))
+
+(defn score [rolls]
+  (add-up 
+    (map add-up (make-game rolls))))
